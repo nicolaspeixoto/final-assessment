c6aa40951bada80988f3089b442a89c77da76c4f
Nicolas <nicolas.santos@adp.com>
2017-05-03 02:01:24 -0300

    adding php

---
+++ php/finalassessment/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Storage;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/{id}', function ($id) {
+     return response()->file('tmp/'.$id);;
+});
+
+Route::post('/{id}', function ($id) {
+    Input::file('data')->move('tmp', $id);
+	return 'ok';
+});
+
+Route::delete('/{id}', function ($id) {
+	unlink('/tmp/'.$id);
+	return 'ok';
+});
+
+Route::post('/', function (Request $request) {
+    $id = uniqid();
+    Input::file('data')->move('tmp', $id);
+    return $id;
+});
